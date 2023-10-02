54990eb817fd5212e66ba01601704dbe0b21f19e
marclachartre <marc.lachartre@gmail.com>
2023-10-02 16:59:31 +0200

    Fix app/api/recettes/[_id]/route.ts in __tests__/api.test.tsx to test right values

--- app/api/recettes/[_id]/route.ts
+++ app/api/recettes/[_id]/route.ts
@@ -1,11 +1,12 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest} from 'next';
 import { RecipeController } from '../../../../controllers/recipe-controller';
 
-export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
-	// console.log(req.query);
-	res.setHeader('Content-Type', 'application/json');
+import { NextResponse } from 'next/server';
+
+export const GET = async (req: NextApiRequest) => {
 	const recipe = await new RecipeController().show(`${req.query._id}`);
-	res.status(200).json(recipe);
+	
+    return NextResponse.json({ recipe })
 };
 
 export default GET;
