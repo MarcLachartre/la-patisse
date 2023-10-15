c54fb8e2b2e925adf45698e032d7344861d1c42a
marclachartre <marc.lachartre@gmail.com>
2023-10-15 19:38:52 +0200

    Fix app/api/recettes/[_id]/route.ts type to from NextApiRequest to NextRequest

--- app/api/recettes/[_id]/route.ts
+++ app/api/recettes/[_id]/route.ts
@@ -1,10 +1,10 @@
-import { NextApiRequest} from 'next';
+import { NextRequest } from 'next/server';
 import { RecipeController } from '../../../../controllers/recipe-controller';
 
 import { NextResponse } from 'next/server';
 
 // api route: /api/recettes/[_id]
-export const GET = async (req: NextApiRequest, { params }: { params: { _id: string } }) => {
+export const GET = async (req: NextRequest, { params }: { params: { _id: string } }) => {
 	const recipe = await new RecipeController().show(`${params._id}`);
 
     return NextResponse.json({ recipe })
