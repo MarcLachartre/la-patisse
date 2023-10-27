cd05e20589c2b1616a1175bbe0e470639b3d12bc
marclachartre <marc.lachartre@gmail.com>
2023-10-27 09:55:54 +0200

    Add POST route to create recipe

--- app/api/recettes/create/route.ts
+++ app/api/recettes/create/route.ts
@@ -1,12 +1,9 @@
-import { NextRequest, NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+import { RecipeController } from "controllers/recipe-controller"
 
-export const POST = async (req: Request, res: NextResponse) => {
-    // console.log(req)
-    // return Response.json({cul:"cul"})
-    return NextResponse
-    // .json({
-    //     cul: "cul"
-    //   }, {
-    //     status: 200,
-    //   })
+export const POST = async (req: NextRequest) => {
+    const recipeToInsert = req.body
+    console.log(req)
+    // const response = await new RecipeController().create(recipeToInsert)
+    return Response.json({cul: "cul" })
 }
