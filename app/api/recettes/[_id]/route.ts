1d5a9927f05f1353bc3728038fb4ed1a76072fa8
marclachartre <marc.lachartre@gmail.com>
2024-03-13 23:08:23 +0100

    Fix api/recettes/_id export

--- app/api/recettes/[_id]/route.ts
+++ app/api/recettes/[_id]/route.ts
@@ -2,8 +2,12 @@ import { NextRequest, NextResponse } from 'next/server';
 import { RecipesController } from '../../../../controllers/recipes-controller';
 
 // api route: /api/recettes/[_id]
-export const GET = async ({ params }: { params: { _id: string } }) => {
+
+const GET = async ({ params }: { params: { _id: string } }) => {
+    console.log(NextRequest);
     const recipe = await new RecipesController().show(`${params._id}`);
 
     return NextResponse.json({ recipe });
 };
+
+export default GET;
