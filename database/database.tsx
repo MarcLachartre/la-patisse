8e045317b0865c68b00a15dee41aaaeb17ba818f
marclachartre <marc.lachartre@gmail.com>
2024-04-04 13:19:53 +0200

    Fix uses atlas db

--- database/database.tsx
+++ database/database.tsx
@@ -27,10 +27,10 @@ class Database {
 
                 const client = await new MongoClient(
                     // Local mongo client
-                    `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/myCakes?authSource=${process.env.DB_AUTH}`,
+                    // `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/myCakes?authSource=${process.env.DB_AUTH}`,
 
                     // Atlas mongo client
-                    // `mongodb+srv://${process.env.DB_ATLAS_USER}:${process.env.DB_ATLAS_PASS}@${process.env.DB_ATLAS_HOST}/myCakes?authSource=${process.env.DB_ATLAS_AUTH}`,
+                    `mongodb+srv://${process.env.DB_ATLAS_USER}:${process.env.DB_ATLAS_PASS}@${process.env.DB_ATLAS_HOST}/myCakes?authSource=${process.env.DB_ATLAS_AUTH}`,
                     options
                 );
                 global._mongoClient = await client.connect();
