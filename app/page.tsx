36854c42a57edbbff60449809fc59151c0811639
marclachartre <marc.lachartre@gmail.com>
2024-04-11 10:20:29 +0200

    seo work

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,16 @@ export const metadata: Metadata = {
     title: 'La Pâtisse 🍰',
     applicationName: 'La Pâtisse 🍰',
     description:
-        "La Pâtisse n'est rien d'autre qu'un simple recueil de recettes pros et persos Elle est née de ma volonté d'en faire l'inventaire et de la partager😊🍰.",
+        "La Pâtisse n'est rien d'autre qu'un simple recueil de recettes pros et persos. Elle est née de ma volonté d'en faire l'inventaire et de la partager😊🍰.",
     creator: 'Marc Lachartre',
     keywords:
-        'La Pâtisse, pâtisserie, delicatessen, recette, recettes, gateaux, entremets, cake',
+        'La Pâtisse, pâtisserie, delicatessen, recette, recettes, gateaux, entremets, cake, tarte, CAP, chef',
 
     openGraph: {
         title: 'La Pâtisse 🍰',
         images: 'https://res.cloudinary.com/dgi1q0deg/image/upload/v1697644670/La%20Patisse/1710874863717.png',
         description:
-            "La Pâtisse n'est rien d'autre qu'un simple recueil de mes recettes favorites. Elle est née de ma volonté d'en faire l'inventaire et de la partager 😊🍰.",
+            "La Pâtisse n'est rien d'autre qu'un simple recueil de recettes pros et persos. Elle est née de ma volonté d'en faire l'inventaire et de la partager😊🍰.",
     },
 };
 
@@ -52,10 +52,12 @@ const Page = () => {
             <div className={styles.homepageDescriptionContainer}>
                 <h1>La Pâtisse</h1>
                 <h5>
-                    Bienvenue! La Pâtisse est un simple recueil des recettes de
-                    gâteaux et autres delicatessen dont j'ai eu la chance de
-                    profiter toute mon enfance et que je compte enrichir de mes
-                    différentes découvertes pâtissières! 😊🍰 Curieux?
+                    Bienvenue! Entre recettes professionelles et personnelles,
+                    La Pâtisse n'est pas qu'un simple recueil de recettes, c'est
+                    la volonté de partager les souvenirs d'une enfance
+                    gourmande, des découvertes pâtissières mais aussi des
+                    classiques de la pâtisserie francaise avec des recettes de
+                    chefs 🍰🧑‍🍳!
                 </h5>
                 <div className={styles.buttonContainer}>
                     <Link href="/recettes">
